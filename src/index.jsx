3d16509518c6faf4ec3fe3a60b25f8266720d8d0
audishos <audisho.sada@gmail.com>
2017-10-02 14:39:05 -0400

    Implements layout as  react components

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 // Application entrypoint.
 
 // Load up the application styles
-require("../styles/application.scss");
+require('../styles/application.scss');
 
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(<MessageList />, document.getElementById('message-list'));
+ReactDOM.render(<Message />, document.getElementById('messages'));
+ReactDOM.render(<ChatBar />, document.getElementById('chat-bar'));
