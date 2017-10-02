d16a04382c7f108fa87ccd1236b57ffeccad7f42
audishos <audisho.sada@gmail.com>
2017-10-02 16:27:43 -0400

    Refactors jsx modules to use the correct structure

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
     return (
-      <div className="message">
-        <span className="message-username">Anonymous1</span>
-        <span className="message-content">I won't be impressed with technology until I can download food.</span>
-      </div>
+      <main className="messages">
+        <Message />
+      </main>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 // Application entrypoint.
 
 // Load up the application styles
-require('../styles/application.scss');
+require("../styles/application.scss");
 
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessageList from './MessageList.jsx';
-import Message from './Message.jsx';
-import ChatBar from './ChatBar.jsx';
+import App from './App.jsx';
 
-ReactDOM.render(<MessageList />, document.getElementById('message-list'));
-ReactDOM.render(<Message />, document.getElementById('messages'));
-ReactDOM.render(<ChatBar />, document.getElementById('chat-bar'));
+ReactDOM.render(<App />, document.getElementById('react-root'));
